1de60f1cff92f662ce2feaf80927efd5cb346691
Maxwell Silva <maxwell.ssilvva@gmail.com>
2022-02-02 20:32:42 -0500

    Criação das telas de Settings e Card

---
+++ FiPay E-Wallet/FiPay/Controllers/CreateViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CreateViewController.swift
+//  FiPay
+//
+//  Created by user209844 on 02/02/22.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ FiPay E-Wallet/FiPay/Controllers/SignViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignViewController.swift
+//  FiPay
+//
+//  Created by user209844 on 02/02/22.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
